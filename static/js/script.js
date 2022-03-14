6f1ab53b4bf0501116c286019d3a91fec5a2b8db
Ishika <mittalishika1801@gmail.com>
2022-03-14 21:18:15 +0530

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -1,10 +1,19 @@
 let allimages = document.querySelectorAll(".image");
+let arr = [];
 for(let i=0;i<allimages.length;i++){
     allimages[i].innerHTML = allimages[i].id;
 }
 for(let i=0;i<allimages.length;i++){
     let image = allimages[i];
     image.addEventListener("click", ()=>{
+        if(arr.includes(image.id)){
+            let pos = arr.indexOf(image.id);
+            arr.splice(pos,1);
+        }
+        else{
+            arr.push(image.id);
+        }    
         image.classList.toggle("selected");
+        console.log(arr);
     })
 }
